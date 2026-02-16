d2e8f0e487e18b6c9dad38870d449d7834f42490
Rachit Singh <rachit.singh.bbps.1@gmail.com>
2026-02-16 22:03:58 +0530

    Error fix

--- app.py
+++ app.py
@@ -2,4 +2,4 @@
 # Churn -> 1 = Yes 0 = No
 # Scaler is imported in the form of = scaler.pkl
 # Model is exported as model.pkl
-# order of the X -> {}'Age', 'Gender', 'Tenure', 'MonthlyCharges']
+# order of the X -> 'Age', 'Gender', 'Tenure', 'MonthlyCharges']
