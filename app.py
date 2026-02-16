7ac5595aaf8d126e18b98af9214bfd95cd46c3c3
Rachit Singh <rachit.singh.bbps.1@gmail.com>
2026-02-16 22:03:36 +0530

    Added comments for context of the data

---
+++ app.py
@@ -0,0 +1,5 @@
+#Gender -> 1 Female 0 Male
+# Churn -> 1 = Yes 0 = No
+# Scaler is imported in the form of = scaler.pkl
+# Model is exported as model.pkl
+# order of the X -> {}'Age', 'Gender', 'Tenure', 'MonthlyCharges']
